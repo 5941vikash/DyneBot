9f86fe781d66ad53263173df56ec172c3c35a18a
5941vikash <vikash7t7t@gmail.com>
2023-12-28 14:35:12 +0530

    Nav Page Folder Created

--- frontend/src/Components/Navbar/NavPages.js
+++ frontend/src/Components/Navbar/NavPages.js
@@ -1,34 +1,43 @@
 const navServicesPages = [
     {
         "name": "Web Development",
-        link :"web-development"
+        "link" : "web-development"
     },
     {
         "name": "UI UX Design",
+        "link" : "uiux-design"
     },
     {
         "name": "Cloud Services",
+        "link" : "cloud-services"
     },
     {
         "name": "PWA Development",
+        "link" : "pwa-development"
     },
     {
         "name": "Mobile App Development",
+        "link" : "mobile-app-development"
     },
     {
         "name": "Product Development",
+        "link" : "product-development"
     },
     {
         "name": "Software Testing",
+        "link" : "software-testing"
     },
     {
         "name": "Web API",
+        "link" : "web-api"
     },
     {
         "name": "Databases",
+        "link" : "databases"
     },
     {
         "name": "Reporting",
+        "link" : "reporting"
     },
 ]
 
