3345fd4c45f67944a58c97f9c2d89223c285bb83
5941vikash <vikash7t7t@gmail.com>
2023-12-29 23:30:17 +0530

    Webpage Content Created

--- frontend/src/Pages/WebDevelopment/WebDev.js
+++ frontend/src/Pages/WebDevelopment/WebDev.js
@@ -6,9 +6,30 @@ const webDevServices = [
         "text": "Move from traditional stores to the online world with a user-friendly website full of advanced features. We create extensive websites using various platforms, giving your business an edge and making it easier to grow your customer base.",
         "img": personalizeWebDevImg
     },
+   
     {
-        "name": "Personalized Web Development",
-        "text": "Move from traditional stores to the online world with a user-friendly website full of advanced features. We create extensive websites using various platforms, giving your business an edge and making it easier to grow your customer base.",
+        "name": "Web App Development",
+        "text": "Engage skilled web application developers from Web Duality and unveil a vibrant web application to enhance the efficiency of intricate operations, leading to an enriched user experience. Our team creates top-tier web applications with specialized API integration, ensuring a uniform user experience for your customers across diverse platforms.",
+        "img": personalizeWebDevImg
+    },
+    {
+        "name": "Ecommerce Development",
+        "text": "The e-commerce sector stands out as one of the most rapidly expanding industries globally. Propel your retail enterprise into the online domain, boosting overall sales through a sales-centric digital store. Proficient in cutting-edge e-commerce platforms, we construct fully operational online stores that deliver an exceptional shopping journey, empowering merchants to enhance their Return on Investment (ROI)..",
+        "img": personalizeWebDevImg
+    },
+    {
+        "name": "Responsive Web Designing",
+        "text": "As a well-regarded web development entity, we recognize the significance of adaptability in the contemporary digital era, where mobile devices hold sway. Every website and web application in our portfolio adheres to a responsive framework, ensuring a uniform user experience across diverse devices. Opting for responsive design enables you to attract premium traffic from both mobile and desktop platforms..",
+        "img": personalizeWebDevImg
+    },
+    {
+        "name": "Backend Development",
+        "text": "Recognizing the importance of a sturdy backend in a website or web application, we acknowledge the expertise required. Our team of adept developers harnesses leading frameworks and libraries to create potent backend solutions, facilitating seamless data flow. These adaptable backends support the ongoing expansion of an enterprise and enhance the ease of future third-party integrations..",
+        "img": personalizeWebDevImg
+    },
+    {
+        "name": "API Integration",
+        "text": "At Web Duality, our commitment lies in creating web solutions endowed with rich features. This is why the integration of APIs constitutes a fundamental aspect of our web development methodology. Through API integration, we seamlessly embed external digital systems into your website or web application, providing it with essential functionalities. This strategy not only elevates the overall capability of your product but also expedites the time-to-market, leading to swift deployments..",
         "img": personalizeWebDevImg
     },
 ]
